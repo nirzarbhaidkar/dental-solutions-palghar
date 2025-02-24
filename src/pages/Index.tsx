a0e1c48143e36e93abc5bb0d830d801ea03bab9a
Nirzar Bhaidkar <n.bhaidkar@gmail.com>
2025-02-24 19:58:06 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -35,6 +35,29 @@ const Index = () => {
     return () => clearInterval(timer);
   }, []);
 
+  useEffect(() => {
+    // Meta Pixel Code
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1358034681882804');
+    fbq('track', 'PageView');
+
+    const noscript = document.createElement('noscript');
+    const img = document.createElement('img');
+    img.height = 1;
+    img.width = 1;
+    img.style.display = 'none';
+    img.src = "https://www.facebook.com/tr?id=1358034681882804&ev=PageView&noscript=1";
+    noscript.appendChild(img);
+    document.body.appendChild(noscript);
+  }, []);
+
 
 
   const services = [
