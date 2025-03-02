a3173481ef9a3ba0a14b32ef8b47ba6f65208d26
Nirzar Bhaidkar <n.bhaidkar@gmail.com>
2025-03-03 00:01:28 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -45,7 +45,7 @@ const Index = () => {
     {
       title: "Cosmetic Dentistry",
       description: "Transform your smile with our expert cosmetic services",
-      image: "https://images.unsplash.com/photo-1609840112990-4265448268d1?ixlib=rb-4.0.3&auto=format&fit=crop&w=1170&q=80"
+      image: "https://plus.unsplash.com/premium_photo-1674998790004-1c08a997011c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       title: "Orthodontics",
